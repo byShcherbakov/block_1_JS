b03aaf68667199a0d403e14cd6423bc52e39b723
byShcherbakov <ilya.shcherbakov.05@list.ru>
2026-04-01 15:43:18 +0300

    Number

---
+++ number.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')(); 
+let x = Number(prompt("Введите 1 ое число "));
+let y = Number(prompt("Введите 1 ое число "));
+let xx = x+y;
+console.log(`Сумма: ${x} + ${x} = ${xx} `);
