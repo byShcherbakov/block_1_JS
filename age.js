2e6c0787bedbc46db9e74d0dd1679c5c30359a8e
byShcherbakov <ilya.shcherbakov.05@list.ru>
2026-04-03 20:29:09 +0300

    arrMonths

--- age.js
+++ age.js
@@ -1,6 +1,6 @@
 //Выведите в консоль своё имя и возраст.
 const prompt = require('prompt-sync')(); 
-let name = String(prompt("Введите имя "));
+let name = String(prompt("Введите имя \n"));
 let age = Number(prompt("Введите возраст "));
 
 console.log(`Вас зовут:${name} \nВаш возраст:${age} `)
